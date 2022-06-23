dee9246492a77aacd9866396e3e3eab5004a0c57
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-23 05:06:32 +0300

    state

--- react-chalenges/src/App.js
+++ react-chalenges/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('Mehmet')
+  const [age, setAge] = useState(31)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Merhaba {name}!</h1>
+      <h2>My Years Old I s{age}</h2>
+      <button onClick={() => setName("Ahmet")}>Change name</button>
+      <button onClick={() => setAge(18)}>Change age </button>
     </div>
   );
 }
