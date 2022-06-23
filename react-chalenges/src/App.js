d6950f176415d7ff0f87263775a2217e13524231
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-23 05:47:28 +0300

    use object state

--- react-chalenges/src/App.js
+++ react-chalenges/src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [name, setName] = useState('Mehmet')
   const [age, setAge] = useState(31)
   const [friends, setFriends] = useState(["Ahmet","Murat"])
+  const [address, setAddress] = useState ({title: 'Bursa', zipcode:21640})
   return (
     <div className="App">
       <h1>Merhaba {name}!</h1>
@@ -17,6 +18,19 @@ function App() {
       {friends.map((friends, index)=> (<div key={index}>{friends}</div>))}
       <br />
       <button onClick={() => setFriends([...friends,"Ayse"])}>Change age </button>
+
+
+      <hr />
+      <br />
+      <h2>Address</h2>
+      <div>{address.title} {address.zipcode}</div>
+
+      <br />
+      {/* onceki veriyi korumak adina ...ADDRESS  Eklenildi*/}
+      <button onClick={()=> setAddress({...address, title:'Gorukle' })} >Change Address</button>
+      {/* <button onClick={()=> setAddress({title:'Gorukle', zipcode: 33333 })} >Change Address</button> */}
+
+
 
     </div>
     
