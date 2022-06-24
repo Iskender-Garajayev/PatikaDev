5423b0975a8c21876fce622ec4e2cf015d43242b
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-24 23:58:06 +0300

    Phonbook app

---
+++ contact-app/src/components/Form/index.js
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+
+function Form() {
+
+  const [form, setForm] = useState({fullname:'', phone_number:''})
+
+  const onChangeInput = (e) =>{
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  
+
+  return (
+    <div>
+      <div>
+        <input name='fullname' placeholder='Full Name' onChange={onChangeInput}/>
+      </div>
+      
+      <div>
+        <input name='phone_number' placeholder='Phone Number' onChange={onChangeInput}/>
+      </div>
+
+      <button>Add</button>
+    </div>
+  )
+}
+
+export default Form
