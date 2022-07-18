d83c9272ca2aaf5f1efb9214171a78942da844dc
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-19 00:52:16 +0300

    uodate

---
+++ JS-Lessons/lesson 4/index.js
@@ -0,0 +1,21 @@
+
+// Karşılaştırma Operatörleri ve Mantıksal Operatörler
+
+
+
+let price = '100'
+let user = 'Ahmet'
+
+//== Eshittir 
+
+console.log(price == 1)
+console.log(price == 100)
+
+//=== hem deger hem de turu esitse
+
+console.log(price===1)
+console.log(price===100)
+
+// != esit degilse
+
+console.log(user != 'gest')
