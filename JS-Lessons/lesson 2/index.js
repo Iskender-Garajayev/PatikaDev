bbe843f317ec4b09bfd5673da7777a22ed5d7684
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-18 23:02:20 +0300

    add new item last and first element

--- JS-Lessons/lesson 2/index.js
+++ JS-Lessons/lesson 2/index.js
@@ -1,3 +1,12 @@
-
 let item = document.querySelector("ul#list>li:last-child");
-console.log(item)
+item.innerHTML = "last child changed";
+console.log(item);
+
+let ulDom = document.querySelector("ul#list");
+let liDom = document.createElement("li");
+
+liDom.innerHTML = "yeni item";
+
+
+ulDom.append(liDom); // create new item last child
+// ulDom.prepend(liDom); create new item first child
