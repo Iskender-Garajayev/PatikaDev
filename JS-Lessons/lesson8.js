8e76aca96c9594f5d01d9f674692b685aaf75a0a
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-18 06:18:28 +0300

    dom

--- JS-Lessons/lesson8.js
+++ JS-Lessons/lesson8.js
@@ -1,2 +1,8 @@
 let title = document.getElementsByTagName('div')
 console.log(title)
+
+let fullName = prompt("Lutfen adinizi giriniz: ")
+
+let grit = document.querySelector("#grit")
+
+grit.innerHTML =`${grit.innerHTML} <small style="color:blue" >${fullName}</small>`
