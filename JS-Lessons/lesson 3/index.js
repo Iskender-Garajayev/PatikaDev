ba433d41ace38e399dbb6c2b177e9adfb3ba4df4
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-19 00:17:59 +0300

    add and remove classis form the elements

---
+++ JS-Lessons/lesson 3/index.js
@@ -0,0 +1,10 @@
+
+
+let code = document.querySelector('#code')
+
+code.classList.add('text-primery')//add class
+code.classList.add('text-primery','header')//add 2 class
+
+code.classList.remove('text-primery') //remove class from the element
+
+console.log(code)
