3c6cb0707fde1e0f79f16ae5f4d45ce18456165c
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-22 06:10:22 +0300

    renew

--- react/contact-app/src/components/Form/index.js
+++ react/contact-app/src/components/Form/index.js
@@ -1,41 +1,39 @@
-import React from 'react'
-import {useState,} from 'react'
+import React from "react";
+import { useState } from "react";
 
-const initialFormValues = {fullname:'', phone_number:''}
+function Form({addContacts, contacts}) {
 
-function Form( addContacts, contacts) {
-
-  const [form, setForm] = useState(initialFormValues)
+  const [form, setForm] = useState({fullname: "", phone_number: ""})
 
   const onChangeInput = (e) =>{
-    setForm({ ...form, [e.target.name]: e.target.value })
+    setForm({...form, [e.target.name]: e.target.value })
   }
 
-  const onSubmit = (e) =>{
+  const onSubmit=(e)=>{
     e.preventDefault()
-    if (form.fullname === "" || form.phone_number ===""){
-      return false;
+
+    if (form.fullname === "" || form.phone_number === "") {
+      return false
     }
     addContacts([...contacts, form])
-
-    setForm(initialFormValues)
-    
-  } 
-
+    console.log(form)
+  }
 
   return (
     <form onSubmit={onSubmit}>
       <div>
-        <input name='fullname' placeholder='Full Name' value={form.fullname} onChange={onChangeInput}/>
-      </div>
-      
-      <div>
-        <input name='phone_number' placeholder='Phone Number' value={form.phone_number} onChange={onChangeInput}/>
+        <input name="fullname" placeholder="Full Name" onChange={onChangeInput} />
       </div>
 
-      <button>Add</button>
+      <div>
+        <input name="phone_number" placeholder="Phone Number" onChange={onChangeInput}  />
+      </div>
+
+      <div>
+        <button>ADD</button>
+      </div>
     </form>
-  )
+  );
 }
 
-export default Form
+export default Form;
