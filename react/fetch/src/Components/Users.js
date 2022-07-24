53af7bcb9336ae6fefce20fbec6d9b590a9dd940
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-25 02:51:43 +0300

    update

--- react/fetch/src/Components/Users.js
+++ react/fetch/src/Components/Users.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react'
 
 function Users() {
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .than((res) => res.json())
-      .than((users) => console.log(users));
-  }, []);
-  return <div> usres</div>;
+    .than((res) => res.json())
+    .than((users) => console.log(users))
+  }, [])
+  return (
+    <div>Users</div>
+  )
 }
 
-export default Users;
+export default Users
